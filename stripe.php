4edaa8bf55916739daca99158261d4e2980b3242
Johan <johan.tedenmark@hotmail.se>
2016-09-25 00:02:57 +0200

    update

--- stripe.php
+++ stripe.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 2007-2015 PrestaShop
+* 2007-2016 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2015 PrestaShop SA
+*  @copyright 2007-2016 PrestaShop SA
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -28,17 +28,16 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-
 require_once(dirname(__FILE__).'/vendor/autoload.php');
 
-class stripe extends PaymentModule
+class Stripe extends PaymentModule
 {
     private $post_errors = array();
     private $html = '';
 
     public function __construct()
     {
-        $this->name = 'Stripe';
+        $this->name = 'stripe';
         $this->version = '1.0.0';
         $this->author = 'JET';
         $this->bootstrap = true;
